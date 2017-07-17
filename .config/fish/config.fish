f190693e193ad23e18399a25836d9858aa99672a
Meghea Iulian <iulian.meghea@gmail.com>
2017-07-17 10:13:42 +0300

    Fix dircolors iTerm

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,11 @@ if not test -e ~/.config/fish/functions/fisher.fish
 end
 set -g theme_nerd_fonts yes
 set -g theme_color_scheme solarized-dark
-if type -fq dircolors and test -e ~/.dircolors.256
-  eval (dircolors -c ~/.dircolors.256 | sed 's/>&\/dev\/null$//')
+
+function _dircolors_set_256_color
+    if command -s dircolors > /dev/null; and test -e ~/.dircolors.256
+        eval (dircolors -c ~/.dircolors.256 | sed 's/>&\/dev\/null$//')
+    end
 end
+
+_dircolors_set_256_color
