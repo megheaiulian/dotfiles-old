815a762e79eac835591bb8f7f74e6728ed0cef35
Meghea Iulian <iulian.meghea@gmail.com>
2018-02-09 08:04:21 +0200

    Remove nixos

--- .nixos/pkgs/all.nix
+++
@@ -1,9 +0,0 @@
-{ config, lib, pkgs, ... }:
-{
-  imports = [
-    ./applications/version-management/git.nix
-    ./applications/version-management/gitkraken.nix
-    ./applications/editors/vim.nix
-    ./applications/misc/tilix.nix
-  ];
-}
